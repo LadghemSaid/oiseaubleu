a5808313db915beb4dfb8aa8371d5a8ef3248305
Nemmiche Loane <loane_nemmiche@ens.univ-artois.fr>
2019-10-03 16:36:20 +0200

    Ajout de la pagination

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -6,6 +6,7 @@ use App\Entity\Article;
 use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 class ArticleController extends AbstractController
 {
@@ -16,16 +17,34 @@ class ArticleController extends AbstractController
     }
 
     /**
-     * @Route("/articles", name="article.index")
+     * Liste l'ensemble des articles triés par date de publication pour une page donnée.
+     *
+     * @Route("/articles/page/{page}", requirements={"page" = "\d+"}, name="article.index")
+     * @Template("XxxYyyBundle:Front/Article:index.html.twig")
+     *
+     * @param int $page Le numéro de la page
+     *
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function index()
+    public function index($page)
     {
-        $articles = $this->repository->findAllVisible();
+        $nbArticlesParPage = 9;
+
+        $articles = $this->repository->findAllPagineEtTrie($page,$nbArticlesParPage);
         $catedories = Article::CATEGORIE;
+
+        $pagination = array(
+            'page' => $page,
+            'nbPages' => ceil(count($articles)/ $nbArticlesParPage),
+            'nomRoute' => 'article.index',
+            'paramsRoute' => array()
+            );
 
         return $this->render('article/index.html.twig', [
             'current_menu' => 'articles',
             'articles' => $articles,
+            'pagination' => $pagination,
             'categories' => $catedories,
         ]);
     }
