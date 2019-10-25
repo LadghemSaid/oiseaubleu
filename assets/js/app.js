6ba4cc2c36a611438dcfa1dc10a1450dbecd5303
Said <said.ladghem@gmail.com>
2019-10-25 10:39:29 +0200

    ajout des repertoires

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,9 +6,8 @@
  */
 
 // any CSS you require will output into a single css file (app.css in this case)
-require('../css/app.css');
-require('../css/home.scss');
-require('../css/all.css');
+require('../css/scss/imports.scss');
+require('../css/scss/helpers/typo/all.css');
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
 // const $ = require('jquery');
